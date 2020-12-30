30ad7b87b6a1b8a0a3459d976849dba09a2ad96e
bg-shorthand <hanhanhan333@gmail.com>
2020-12-30 23:28:10 +0900

    MVC

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-const $form = document.querySelector('form');
-const $inputTodo = document.querySelector('.input-todo');
-const $addBtn = document.querySelector('.add-btn');
+let model = [];
+
 const $todos = document.querySelector('.todos');
-const $body = document.querySelector('body');
-const $delete = document.querySelector('.delete');
+const $form = document.querySelector('form');
+const $input = document.querySelector('.input-todo');
 
-function addTodo () {
-  const $todo = document.createElement('li');
-  const $checkBox = document.createElement('input');
-  const $delete = document.createElement('button');
-  
-  $todo.textContent = $inputTodo.value;
-  $todo.classList.add('todo')
-  
-  $checkBox.setAttribute('type', 'checkbox')
-  $todo.insertBefore($checkBox, $todo.firstChild);
-  
-  $delete.textContent = 'X';
-  $todo.appendChild($delete);
-  $delete.classList.add('delete');
-  
-  $todos.appendChild($todo);
+(function () {
+  //서버에서 자료를 받음
+  model = [
+    { id: 1, content: 'html', completed: true},
+    { id: 2, content: 'css', completed: false},
+    { id: 3, content: 'javascript', completed: true}
+  ]
+})();
+
+const render = () => {
+  if ($todos.hasChildNodes) [...$todos.childNodes].forEach(v => $todos.removeChild(v));
+
+  model.forEach(({ id, content, completed }) => {
+    const $todo = document.createElement('li');
+    const $checkbox = document.createElement('input');
+    const $span = document.createElement('span');
+    const $button = document.createElement('button');
+    
+    $checkbox.setAttribute('type', 'checkbox');
+    $button.textContent = 'X';
+    $button.setAttribute('type', 'button');
+
+    $todo.setAttribute('id', +id);
+    $todo.classList.add('todo');
+    $checkbox.checked = completed;
+    $span.style.textDecoration = $checkbox.checked ? 'line-through' : 'none';
+    
+    $span.textContent = content;
+    $todo.appendChild($checkbox);
+    $todo.appendChild($span);
+    $todo.appendChild($button);
+    
+    $todos.appendChild($todo);
+  })
 }
 
-function changeColor () {
-  const random1 = Math.floor(Math.random() * 255 + 1)
-  const random2 = Math.floor(Math.random() * 255 + 1)
-  const random3 = Math.floor(Math.random() * 255 + 1)
-
-  $body.style.backgroundColor = `rgb(${random1},${random2},${random3})`
-}
+document.addEventListener('DOMContentLoaded', render);
 
 $form.onsubmit = e => {
   e.preventDefault();
-  if ($inputTodo.value) {
-    addTodo();
-    changeColor();
-    $inputTodo.value = '';
-    $inputTodo.focus();
-  }
+  if (!$input.value) return;
+  model = [{ id: Math.max(...model.map(v => v.id)) + 1, content: $input.value, completed: false }, ...model]
+  render();
+  $input.value = '';
+  $input.focus();
 }
 
-$delete.onclick = () => {
-  console.log($delete.parentElement);
+$todos.onclick = e => {
+  if (e.target.getAttribute('type') !== 'button') return;
+  model = model.filter(v => v.id !== +e.target.parentNode.getAttribute('id'));
+  render();
+}
+
+$todos.onchange = e => {
+  // if (e.target.getAttribute('type') !== 'checkbox') return;
+  model = model.map(v => +e.target.parentNode.getAttribute('id') === v.id ? { ...v, completed: !v.completed } : v);
+  render();
 }
