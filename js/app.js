f9703e40fa56c637a0a2ef17cc1484438b6791cf
bg-shorthand <hanhanhan333@gmail.com>
2021-01-02 12:40:58 +0900

    feat: change active class

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let todos = [];
+let activeTodos = [];
+let completedTodos = [];
 
 const $todos = document.querySelector('.todos');
 const $inputTodo = document.querySelector('.input-todo');
@@ -6,6 +8,10 @@ const $completeAll = document.getElementById('ck-complete-all');
 const $clearCompletedButton = document.querySelector('.clear-completed > .btn');
 const $completedTodos = document.querySelector('.completed-todos');
 const $activeTodos = document.querySelector('.active-todos');
+const $nav = document.querySelector('.nav');
+const $all = document.getElementById('all');
+const $active = document.getElementById('active');
+const $completed = document.getElementById('completed');
 
 (() => {
   // 서버에서 자료 받아옴
@@ -16,12 +22,25 @@ const $activeTodos = document.querySelector('.active-todos');
   ];
 })();
 
+const viewTodos = () => {
+  activeTodos = todos.filter(todo => !todo.completed);
+  completedTodos = todos.filter(todo => todo.completed);
+};
+
 const render = () => {
   if ($todos.hasChildNodes()) [...$todos.childNodes].forEach(todo => $todos.removeChild(todo));
 
+  let model = [];
+
+  viewTodos();
+
+  if ($all.classList.contains('active')) model = todos;
+  else if ($active.classList.contains('active')) model = activeTodos;
+  else if ($completed.classList.contains('active')) model = completedTodos;
+
   const $fragment = document.createElement('fragment');
- 
-  todos.forEach(({ id, content, completed }) => {
+
+  model.forEach(({ id, content, completed }) => {
     const $todo = document.createElement('li');
     $todo.setAttribute('id', id);
     $todo.classList.add('todo-item');
@@ -46,54 +65,62 @@ const render = () => {
 
     $fragment.appendChild($todo);
     $todos.appendChild($fragment);
-  })
+  });
 
-  $completeAll.checked = !todos.map(todo => todo.completed).includes(false)
+  $completeAll.checked = !model.map(todo => todo.completed).includes(false);
   $completedTodos.textContent = todos.filter(todo => todo.completed).length;
-  $activeTodos.textContent = todos.length - $completedTodos.textContent
-}
+  $activeTodos.textContent = todos.length - $completedTodos.textContent;
+};
 
 document.addEventListener('DOMContentLoaded', render);
 
 const addTodo = () => {
   todos = [{ id: todos.length ? Math.max(...todos.map(v => v.id)) + 1 : 1, content: $inputTodo.value, completed: false }, ...todos];
   render();
-}
+};
 const removeTodo = target => {
   todos = todos.filter(todo => todo.id !== +target.parentNode.id);
   render();
-}
+};
 const completedTodo = target => {
-  todos = todos.map(todo => +target.parentNode.id === todo.id ? { ...todo, completed: !todo.completed } : todo);
+  todos = todos.map(todo => (+target.parentNode.id === todo.id ? { ...todo, completed: !todo.completed } : todo));
   render();
-}
-const checkingCompleteAll = (target) => {
-  todos = target.checked ? todos.map(todo => ({...todo, completed: true})) : todos.map(todo => ({...todo, completed: false}));
+};
+const checkingCompleteAll = target => {
+  todos = target.checked ? todos.map(todo => ({ ...todo, completed: true })) : todos.map(todo => ({ ...todo, completed: false }));
   render();
-}
+};
 const clearCompleted = () => {
   todos = todos.filter(todo => !todo.completed);
   render();
-}
+};
+const changeActive = e => {
+  [...e.currentTarget.children].forEach(list => list.classList.remove('active'));
+  e.target.classList.add('active');
+  render();
+};
 
 $inputTodo.onkeyup = e => {
   if (e.key !== 'Enter' || !e.target.value) return;
   addTodo();
   $inputTodo.value = '';
-}
+};
 
 $todos.onclick = e => {
   if (!e.target.matches('.todos .remove-todo')) return;
   removeTodo(e.target);
-}
+};
 
 $todos.onchange = e => {
   completedTodo(e.target);
-}
+};
 
 $completeAll.onchange = e => {
   checkingCompleteAll(e.target);
-}
+};
 
 $clearCompletedButton.onclick = clearCompleted;
 
+$nav.onclick = e => {
+  changeActive(e);
+};
